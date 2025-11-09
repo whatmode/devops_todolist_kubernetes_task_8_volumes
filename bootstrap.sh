60dea972371d86780609b7f8bf1ade95b56e93e0
Volodymyr Kolomiiets <valdi6014@gmail.com>
2025-11-09 17:18:43 +0000

    Added what should be applied within bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+kubectl create cluster --config cluster.yml
+kubectl apply -f .infrastructure/namespace.yml
+kubectl apply -f .infrastructure/clusterIp.yml
+kubectl apply -f .infrastructure/nodePort.yml
+kubectl apply -f .infrastructure/secret.yml
+kubectl apply -f .infrastructure/confgiMap.yml
+kubectl apply -f .infrastructure/hpa.yml
+kubectl apply -f .infrastructure/pv.yml
+kubectl apply -f .infrastructure/pvc.yml
+kubectl apply -f .infrastructure/deployment.yml
