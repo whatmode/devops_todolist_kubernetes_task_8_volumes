f4219eb84439a44259e6e231031d558ef3185a48
Volodymyr Kolomiiets <valdi6014@gmai.com>
2025-12-14 22:21:28 +0200

    Fixed  PR comments

--- INSTRUCTION.md
+++ INSTRUCTION.md
@@ -9,7 +9,6 @@ kubectl port-forward pod/<app-pod-name> <local-port>:<clusterIP-target-port> -n 
 kubectl exec -it <app-pod-name> bash
 2. Run command:
 ls -la /configs
-
 # To validate that secret data is mounted as files in the right order
 1. Run command:
 kubectl exec -it <app-pod-name> bash
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ kubectl apply -f .infrastructure/namespace.yml
 kubectl apply -f .infrastructure/clusterIp.yml
 kubectl apply -f .infrastructure/nodePort.yml
 kubectl apply -f .infrastructure/secret.yml
-kubectl apply -f .infrastructure/confgiMap.yml
+kubectl apply -f .infrastructure/configMap.yml
 kubectl apply -f .infrastructure/hpa.yml
 kubectl apply -f .infrastructure/pv.yml
 kubectl apply -f .infrastructure/pvc.yml
